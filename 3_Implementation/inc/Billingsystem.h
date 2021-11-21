185d96f36bf718c837dcccfa675c28a98e9c0153
Sankar Narayanan <91029826+madesh1604@users.noreply.github.com>
2021-11-21 22:14:27 +0530

    header file

--- 3_Implementation/inc/Billingsystem.h
+++ 3_Implementation/inc/Billingsystem.h
@@ -1,1 +1,18 @@
+#ifndef  function.h
+#define  function.h
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+#include<conio.h>
 
+void create();
+void bill();
+void display();
+void edit();
+void invoice ();
+void mdelete();
+void mdisplay();
+
+
+#endif
