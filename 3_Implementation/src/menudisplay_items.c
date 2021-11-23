4c8f12fee2c59ee8fb8e7d8fadbb294d3f8f6527
Sankar Narayanan <91029826+madesh1604@users.noreply.github.com>
2021-11-24 00:33:12 +0530

    Update menudisplay_items.c

--- 3_Implementation/src/menudisplay_items.c
+++ 3_Implementation/src/menudisplay_items.c
@@ -10,6 +10,6 @@ void mdisplay()
 	printf("================================================================================\n");
 	for(o=0;o<ti;o++)
 	{
-		printf("%14d\t|%12s\t|%6d\t|%6d\n",cst[o].productno,cst[o].productname,cst[o].quantity,cst[o].price);
+		printf("%14d\t|%12s\t|%6d\t|%6d\n",customer[o].productno,customer[o].productname,customer[o].quantity,customer[o].price);
 	}
 }
